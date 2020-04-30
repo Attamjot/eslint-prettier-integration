a61b320a626facb6df8f0faec1e5cc428a066566
Attamjot <attamjotsingh91@gmail.com>
2020-04-30 21:58:47 +0530

    First commit to eslint and prettier integration

---
+++ index.js
@@ -0,0 +1,23 @@
+const x = 1;
+
+const name = 'Attamjot';
+
+const printName = () => {
+  console.log('Attamjot');
+};
+
+const print = function () {
+  console.log('Hello');
+};
+
+const obj = {
+  hello: function () {
+    console.log('hello');
+  },
+};
+
+class A {
+  foo() {
+    console.log('Hey class'); /* this is my class */
+  }
+}
